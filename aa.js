080ef1be8ef261e27de21e494985262f6545f29d
Shylaja L <lavudyashailaja123@gmail.com>
2025-05-24 23:50:56 +0530

    Update aa.js

--- aa.js
+++ aa.js
@@ -1,3 +1,66 @@
-let name='joe';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Flipkart Items</title>
+</head>
+<body>
+    <p>Popular Items on Flipkart</p>
+    <ul>
+        <li>Samsung Galaxy S24 Ultra - 12GB RAM, 256GB Storage</li>
+        <li>HP Pavilion x360 Laptop - 11th Gen Intel Core i5</li>
+        <li>boAt Rockerz 450 Bluetooth Headphones</li>
+        <li>Samsung 253L 3 Star Inverter Frost Free Double Door Refrigerator</li>
+        <li>Whirlpool 7 Kg Semi Automatic Top Load Washing Machine</li>
+        <li>Apple iPad 10th Gen (Wi-Fi, 64GB)</li>
+        <li>Nike Revolution 6 Running Shoes</li>
+        <li>Realme Smart TV 32 inch HD Ready LED</li>
+        <li>Philips Hair Dryer - HP8100/60</li>
+        <li>Fire-Boltt Ninja Call Pro Smartwatch with Bluetooth Calling</li>
+    </ul>
+  
+    <p>Popular Food Items on Flipkart</p>
+    <ul>
+        <li>Maggi 2-Minute Instant Noodles - Masala, 560g (Pack of 8)</li>
+        <li>Tata Sampann Unpolished Toor Dal (Arhar), 1kg</li>
+        <li>Fortune Sunflower Oil - Refined, 1L Pouch</li>
+        <li>Lay’s Classic Salted Potato Chips, 52g</li>
+        <li>Cadbury Dairy Milk Silk Chocolate Bar, 150g</li>
+        <li>BRU Instant Coffee, 100g Jar</li>
+        <li>Amul Taaza Toned Milk, 1L Tetra Pack</li>
+        <li>Daawat Basmati Rice – Super, 5kg</li>
+        <li>Quaker Oats Pouch – 1kg</li>
+        <li>Real Fruit Power Mango Juice, 1L</li>
+    </ul>
+  
+    <p>Popular Clothing Items on Flipkart</p>
+    <ul>
+        <li>Men's Cotton Solid Casual Shirt - Allen Solly</li>
+        <li>Women's A-Line Printed Kurti - BIBA</li>
+        <li>Men's Slim Fit Stretch Jeans - Levi's</li>
+        <li>Women's Ankle-Length Leggings - Jockey</li>
+        <li>Men's Polo Neck T-shirt - U.S. Polo Assn.</li>
+        <li>Women's Maxi Dress - Tokyo Talkies</li>
+        <li>Men's Hooded Sweatshirt - Puma</li>
+        <li>Unisex Solid Rain Jacket - Wildcraft</li>
+        <li>Women's Saree with Blouse Piece - Satrani</li>
+        <li>Men's Formal Trousers - Van Heusen</li>
+    </ul>
 
-console.log("Thanks youfro yoer time"+name);
+
+    <p>Popular Sports Items on Flipkart</p>
+    <ul>
+        <li>NIVIA Storm Football - Size 5</li>
+        <li>Yonex GR 303 Badminton Racket with Cover</li>
+        <li>Cosco Fitness Dumbbells Set - 10kg</li>
+        <li>SG RSD Spark Cricket Bat (Size Full)</li>
+        <li>Vector X Skipping Rope with Foam Handles</li>
+        <li>HEAD Tennis Balls (Pack of 3)</li>
+        <li>Adidas Men's Sports T-shirt - Quick Dry</li>
+        <li>Puma Unisex Running Shoes</li>
+        <li>LI-NING Shuttlecocks (Pack of 6)</li>
+        <li>Cosco Yoga Mat - 6mm Anti-Slip</li>
+    </ul>
+
+</body>
+</html>
