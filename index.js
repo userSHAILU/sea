302606dc23f6825afd0c396ffd439b8d53174553
Shylaja L <lavudyashailaja123@gmail.com>
2025-05-27 14:32:17 +0530

    calculatearea
    calculatearea

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let environment= 'prod'
-environment=100;
-if(environmet=='prod')
-{
-    console.log('production mode')
+
+function calculateArea(length, width) {
+  return length * width;
 }
+
+function calculatePerimeter(length, width) {
+  return 2 * (length + width);
+}
+
+let length = 5;
+let width = 3;
+
+let area = calculateArea(length, width);
+let perimeter = calculatePerimeter(length, width);
+
+console.log(`Area: ${area}`);
+console.log(`Perimeter: ${perimeter}`);
